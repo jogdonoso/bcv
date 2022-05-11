860ed6fd49e7eedb8e1b03e50eca08a589fe9206
jogdonoso <105387261+jogdonoso@users.noreply.github.com>
2022-05-11 18:43:48 -0300

    Update bcv.php

--- bcv.php
+++ bcv.php
@@ -1,7 +1,23 @@
 <?php
 header('Content-type: application/json; charset=utf-8');
-$url = "http://www.bcv.org.ve";
-$content = file_get_contents($url);
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'http://www.bcv.org.ve',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+));
+
+$content = curl_exec($curl);
+
+
+//echo $content;
 $posible_usd = substr($content, strpos($content, 'USD'));
 $pos_usd = strpos($posible_usd,' </strong> </div>');
 $posible_usd = substr($posible_usd,0,$pos_usd);
@@ -33,7 +49,7 @@ $posible_cny = substr($posible_cny,0,$pos_cny);
 $posible_cny= substr($posible_cny,34,1000);
 $cotizaciones = array('message_ok' =>
    array('data' =>
-                        array('dollar' => array('value' => str_replace(',','.',substr($posible_usd, -10)),
+                        array('dolar' => array('value' => str_replace(',','.',substr($posible_usd, -10)),
                                     'iso' => 'USD',
                                     'symbol' => '$' ),
                               'euro' => array('value' => str_replace(',','.',substr($posible_eur, -10)),
@@ -45,14 +61,11 @@ $cotizaciones = array('message_ok' =>
                                'lira' => array('value' => str_replace(',','.',substr($posible_try, -10)),
                                     'iso' => 'TRY',
                                     'symbol' => '₺' ),
-                                'ruble' => array('value' => str_replace(',','.',substr($posible_rub, -10)),
+                                'rublo' => array('value' => str_replace(',','.',substr($posible_rub, -10)),
                                     'iso' => 'RUB',
                                     'symbol' => '₽' )
                             )));
       
         echo json_encode($cotizaciones);
-        
-        /*array('dollar' => str_replace(',','.',substr($posible_usd, -10)),
-        'uuro' => str_replace(',','.',substr($posible_eur, -10)),
-        'yuan' => str_replace(',','.',substr($posible_cny, -10))
-      )));*/
+
+      curl_close($curl);
